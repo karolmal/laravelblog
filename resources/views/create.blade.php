dcc1068924a3d22b0b464a501b1bc26082df7eca
Karol <karol@senses.co.uk>
2020-04-21 13:28:03 +0100

    working sending data to database

---
+++ resources/views/create.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.master')
+
+@section('links')
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+         <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav">
+                <a href="{{ route('create') }}">Create</a>
+                <a href="{{ route('view') }}">View</a>
+            </div>
+            <div class="navbar-nav ml-auto">
+                <a href="#" class="nav-item nav-link">Exit</a>
+            </div>
+        </div>
+    </nav>
+@endsection
+
+@section('form')
+
+<div class="container">
+  <h2>Form</h2>
+  <form method="POST" action="/create">
+    <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" class="form-control" placeholder="name" name="name">
+    </div>
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control"placeholder="Enter email" name="email">
+    </div>
+    {{csrf_field()}}
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
+    
+@endsection
+
+
+
+
